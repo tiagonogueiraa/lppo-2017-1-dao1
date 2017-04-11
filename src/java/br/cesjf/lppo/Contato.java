f028ffa38f54f9e4c028e5adc842f03fd6d37963
Tiago Nogueira <tiagonogueirajf@gmail.com>
2017-04-10 21:23:44 -0300

    Adiciona classe Contato

---
+++ src/java/br/cesjf/lppo/Contato.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cesjf.lppo;
+
+/**
+ *
+ * @author Tiago Nogueira
+ */
+public class Contato {
+
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+
+    public Contato() {
+        //para garantir que os valores comecem com null
+        id = null;
+        nome = null;
+        sobrenome = null;
+        telefone = null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
